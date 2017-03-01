5df0370d6b4b89a802712ae78255e5c392f0f61f
tonny zambrano <tonnyzambrano21@gmail.com>
2017-03-01 13:32:42 +0000

    make tinyturtle

---
+++ script.js
@@ -0,0 +1,17 @@
+    TinyTurtle.apply(window);
+    forward (50);
+    right (90);
+    forward (45);
+    right (90);
+    forward (50);
+    right (90);
+    forward (45);
+    right (90);
+    forward (50);
+    right (45);
+    forward (32);
+    right (90);
+    forward (32);
+
+
+
